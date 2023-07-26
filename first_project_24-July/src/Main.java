dddc3e6d1839ca9c3dcccb0b6f8c1b9a9f6f6b2e
Safa Sulaiman Alhussaini <111584212+SafaAlhusaini@users.noreply.github.com>
2023-07-26 16:14:19 +0400

    Add files via upload

---
+++ first_project_24-July/src/Main.java
@@ -0,0 +1,140 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+
+
+//print out
+//--------------
+//        System.out.println("Safa");
+//        System.out.println(100.5);
+//        System.out.println(5);
+//        System.out.println(4/5);
+//        System.out.println(true);
+//        System.out.println(false);
+
+//        // ---------------------------------------
+//        System.out.println(10 + 5);
+//        System.out.println(5/2); //because its integer for that it shows int value
+//        System.out.println(10-8);
+//        System.out.println(4*6);
+//        System.out.println(5.0/2);
+
+//        //----------------------------------------
+//        System.out.println(2==5);
+//        System.out.println(6!=5);
+
+//        //----------------------------------------------- Task 1 -- Spaces
+//        System.out.println(" J   a      v         a");
+//        System.out.println("J J aaa    vvv       aaa");
+//        System.out.println("   aaaaa  vvvvv     aaaaa");
+//        System.out.println("         vvvvvvv   aaaaaaa");
+//        System.out.println("                  aaaaaaaaa");
+
+//        //------------------------------------------------ Task 2
+//        System.out.println(" --- --- ---");
+//        System.out.println("|   |   |   |");
+//        System.out.println(" --- --- ---");
+//        System.out.println("|   |   |   |");
+//        System.out.println(" --- --- ---");
+//        System.out.println("|   |   |   |");
+//        System.out.println(" --- --- ---");
+
+//        //----------------------------------------------- Task 3
+//        System.out.println(4.0 * (1-(1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));
+
+//        //----------------------------------------------- Task 4
+//        System.out.println("o o o m m m m m m m");
+//        System.out.println("o o o m m m m m m m");
+//        System.out.println("o o o a a a a a a a");
+//        System.out.println("o o o a a a a a a a");
+//        System.out.println("o o o n n n n n n n");
+//        System.out.println("o o o n n n n n n n");
+
+        //----------------------------------------------
+//        int x;
+//        x = 26;
+//        x = x + 1;
+//        double weight = 65.0;
+//        boolean doing_workout = true;
+//        String name = "Safa";
+//        System.out.println(x);
+
+//print in
+//--------------
+//        Scanner read = new Scanner(System.in);
+//
+//        System.out.println("Enter your name :");
+//        String name = read.next();
+//
+//        System.out.println("Welcome " +  name);
+
+        //------------------------------------------- Task 5
+//        Scanner read = new Scanner(System.in);
+//        System.out.println("Welcome to Our System");
+//        System.out.println("Enter your name :");
+//        String name = read.next();
+//        System.out.println("your age :");
+//        int age = read.nextInt();
+//        age = 100 - age;
+//        System.out.println( " Mr/Ms " + name + " still you have " + age + " years   to become a 100 years old ");
+
+        //-------------------------------------------- Task 6
+//        Scanner read = new Scanner(System.in);
+//        System.out.println(" Enter a fist numeric number :");
+//        double x = read.nextDouble();
+//        System.out.println(" Enter a Sec numeric number :");
+//        double z = read.nextDouble();
+//        System.out.println(" * oper = " + (x * z));
+//        System.out.println(" + oper = " + (x + z));
+//        System.out.println(" / oper = " + (x / z));
+//        System.out.println(" - oper = " + (x - z));
+
+        //------------------------------------------- Task 7
+
+//        double r = 7.5;
+//        double Area = 3.14 * (r * r);
+//        double Perimeter = 2 * r * 3.14;
+//        System.out.println("The Area of Circl : " + Area );
+//        System.out.println("The Perimeter of Circle :  " + Perimeter);
+
+      //--------------------------------------------- Task 8
+//        Scanner read = new Scanner(System.in);
+//        System.out.println("----Average Calculater ----");
+//        System.out.println("Enter First Number : ");
+//        int s = read.nextInt();
+//        System.out.println("Enter Sec Number : ");
+//        int a = read.nextInt();
+//        System.out.println("Enter third Number : ");
+//        int f = read.nextInt();
+//        double Average = (s + a + f) / 3;
+//        System.out.println("The Average = " + Average);
+
+      //-------------------------------------------- Task 9
+//        double width = 5.5;
+//        double height = 8.5;
+//        double Area = width * height;
+//        double Per = 2 * (5.6 * 8.5);
+//        System.out.println(" Area = " + Area);
+//        System.out.println(" Perimeter = " + Per );
+
+       //------------------------------------------- Tsk 10
+
+        double S = 9.8;
+        double M = 10.8;
+        double Z ;
+        System.out.println(M + " ," + S);
+        Z = S;
+        S = M;
+        M = Z;
+        System.out.println(M + " ," + S);
+
+        
+                                                  //----------------- 24 / JULY / 2023 ----------------------//
+
+
+    }
+}
