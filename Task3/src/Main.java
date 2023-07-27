0f9a309b95cd90fa8df4a0d8a1d5590b83dd4aef
Safa Sulaiman Alhussaini <111584212+SafaAlhusaini@users.noreply.github.com>
2023-07-27 07:41:39 +0400

    Add files via upload

---
+++ Task3/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+//Task 1
+//        int rows=5;
+//
+//        for (int i= 0; i<= rows; i++)
+//        {
+//
+//            for (int j=1; j<=rows-i; j++)
+//            {
+//                System.out.print(" ");
+//            }
+//            for (int s=0;s<=i;s++)
+//            {
+//                System.out.print("*");
+//            }
+//            System.out.println("");
+//        }
+
+
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("Calculator");
+        System.out.println("Enter number :");
+        int num1 = read.nextInt();
+
+        System.out.println("Enter number :");
+        int num2 = read.nextInt();
+
+        switch() {
+            case x:
+                // code block
+                break;
+            case y:
+                // code block
+                break;
+            default:
+                // code block
+        }
+    }
+
+
+}
