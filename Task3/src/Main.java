172211e7c7a22966310d603f33bdc8066549fbe7
Safa Sulaiman Alhussaini <111584212+SafaAlhusaini@users.noreply.github.com>
2023-07-27 10:52:47 +0400

    Update Main.java

--- Task3/src/Main.java
+++ Task3/src/Main.java
@@ -25,22 +25,52 @@ public class Main {
         Scanner read = new Scanner(System.in);
 
         System.out.println("Calculator");
-        System.out.println("Enter number :");
-        int num1 = read.nextInt();
+for( int i = 0 ; i < 10 ; i++) {
+    System.out.println("Chose your Operator : * ,/ ,+, -");
+    String Operator = read.next();
 
-        System.out.println("Enter number :");
-        int num2 = read.nextInt();
+    System.out.println("Enter number :");
+    int num1 = read.nextInt();
 
-        switch() {
-            case x:
-                // code block
-                break;
-            case y:
-                // code block
-                break;
-            default:
-                // code block
+    System.out.println("Enter number :");
+    int num2 = read.nextInt();
+
+    switch (Operator) {
+        case "+":
+            System.out.println(" Total = " + num1 + num2);
+            break;
+        case "-":
+            System.out.println(" Total = " + (num1 - num2));
+            break;
+        case "*":
+            System.out.println(" Total = " + num1 * num2);
+            break;
+        case "/":
+            if (num2 == 0) {
+                System.out.println("warning cant divided by 0 ");
+            } else {
+                System.out.println(" Total = " + num1 / num2);
+            }
+            break;
+        case "0":
+
+            break;
+        default:
+            System.out.println("there is no " + Operator + " like this");
+            System.out.println(" write Exit if you want to exit ");
+            String exit = read.next();
+            if (exit.equals("Exit"))
+            {
+                System.out.println("See you next time");
+            }
+            break;
+
+}
+    System.exit(0);
+
+            // code block
         }
+
     }
 
 
