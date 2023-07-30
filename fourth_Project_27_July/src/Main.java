3394a103d6b443b04c749a07e5dfb005ebe69e97
Safa Sulaiman Alhussaini <111584212+SafaAlhusaini@users.noreply.github.com>
2023-07-30 11:45:23 +0400

    Add files via upload

---
+++ fourth_Project_27_July/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+    public static void main(String[] args) {
+//------------------------------------------------------------------------
+        for (int i = 1; i <= 12; i++)
+        {
+            System.out.println("\t");
+             for(int j = 1; j <= 12 ; j++)
+             {
+                 System.out.print( i + "x" + j +"=" + (i * j) + "\t");
+             }
+
+        }
+//-----------------------------------------------------------------------
+
+
+
+//
+
+
+
+
+
+
+    }
+}
